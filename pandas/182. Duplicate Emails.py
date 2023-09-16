19202cafc11768e3c086b50fb755ede0b9072403
Harsh Yadav <55807854+harshyadav1508@users.noreply.github.com>
2023-09-17 01:36:50 +0530

    Update 182. Duplicate Emails.py
    Added better solution

--- pandas/182. Duplicate Emails.py
+++ pandas/182. Duplicate Emails.py
@@ -3,6 +3,12 @@
 import pandas as pd
 
 def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
-  person = person.rename(columns={'email':'Email'})
-  return person[person['Email'].duplicated()]['Email'].to_frame().drop_duplicates()
+  #First solution
+  # person = person.rename(columns={'email':'Email'})
+  # return person[person['Email'].duplicated()]['Email'].to_frame().drop_duplicates()
+
+  #Second solution
+  df = person.loc[person['email'].duplicated(),['email']]
+  return df.drop_duplicates()
+    
 
