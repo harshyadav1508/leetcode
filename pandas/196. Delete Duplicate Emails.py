28437047a89be4e4e168f3dde8236e2f417251aa
Harsh Yadav <55807854+harshyadav1508@users.noreply.github.com>
2023-09-17 17:42:00 +0530

    Create 196. Delete Duplicate Emails.py
    Added solution for Delete Duplicate Emails

---
+++ pandas/196. Delete Duplicate Emails.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/delete-duplicate-emails/?lang=pythondata
+
+import pandas as pd
+
+# Modify Person in place
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    person.sort_values(by='id', inplace=True)
+    person.drop_duplicates(subset="email", keep='first', inplace=True)
+    
