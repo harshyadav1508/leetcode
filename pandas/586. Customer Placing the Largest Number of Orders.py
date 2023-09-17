56bd990c568cb7ab3afc28a3f69451b6e47c3108
Harsh Yadav <55807854+harshyadav1508@users.noreply.github.com>
2023-09-17 18:25:39 +0530

    Create 586. Customer Placing the Largest Number of Orders.py

---
+++ pandas/586. Customer Placing the Largest Number of Orders.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/description/?lang=pythondata
+
+import pandas as pd
+
+def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
+    customer_counts = orders['customer_number'].value_counts()
+    if len(customer_counts)==0:
+        empty_df = pd.DataFrame()
+        empty_df['customer_number'] = []
+        return empty_df
+    return pd.DataFrame({'customer_number': [customer_counts.idxmax()]})
+    
