a8401ac713c2461123d413d1b22915e95b4ceca8
Harsh Yadav <55807854+harshyadav1508@users.noreply.github.com>
2023-09-17 18:27:52 +0530

    Update 586. Customer Placing the Largest Number of Orders.py
    Better solution

--- pandas/586. Customer Placing the Largest Number of Orders.py
+++ pandas/586. Customer Placing the Largest Number of Orders.py
@@ -3,10 +3,11 @@
 import pandas as pd
 
 def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
-    customer_counts = orders['customer_number'].value_counts()
-    if len(customer_counts)==0:
-        empty_df = pd.DataFrame()
-        empty_df['customer_number'] = []
-        return empty_df
-    return pd.DataFrame({'customer_number': [customer_counts.idxmax()]})
+    # customer_counts = orders['customer_number'].value_counts()
+    # if len(customer_counts)==0:
+    #     empty_df = pd.DataFrame()
+    #     empty_df['customer_number'] = []
+    #     return empty_df
+    # return pd.DataFrame({'customer_number': [customer_counts.idxmax()]})
+    return orders["customer_number"].mode().to_frame()
     
