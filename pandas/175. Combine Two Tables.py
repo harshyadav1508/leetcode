46abfd399fefbebae5a3e2d239ffc61e4bfb4a50
Harsh Yadav <55807854+harshyadav1508@users.noreply.github.com>
2023-09-20 00:28:32 +0530

    Update 175. Combine Two Tables.py
    Added better solution

--- pandas/175. Combine Two Tables.py
+++ pandas/175. Combine Two Tables.py
@@ -3,6 +3,10 @@
 import pandas as pd
 
 def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
-    merged_df = pd.merge(person, address, left_on='personId', right_on='personId', how='left',suffixes=('_left', '_right'))
-    return merged_df[['firstName','lastName','city','state']]
+    # merged_df = pd.merge(person, address, left_on='personId', right_on='personId', how='left',suffixes=('_left', '_right'))
+    # return merged_df[['firstName','lastName','city','state']]
+
+    # return pd.merge(person, address, on='personId',how='left')[['firstName','lastName','city','state']]
+    
+    return pd.merge(left=person, right=address, how='left', on='personId')[['firstName', 'lastName', 'city', 'state']]
     
